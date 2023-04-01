8940e7f87028c7e0445160ff5384c269a2a65b55
youssef917 <o@gmail.com>
2023-04-01 18:00:46 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -10,31 +10,31 @@ import {
   CryptoDetails,
   Navbar,
 } from "./components";
-import axios from "axios";
+// import axios from "axios";
 
-const options = {
-  method: 'GET',
-  url: 'https://coinranking1.p.rapidapi.com/coins',
-  params: {
-    referenceCurrencyUuid: 'yhjMzLPhuIDl',
-    timePeriod: '24h',
-    'tiers[0]': '1',
-    orderBy: 'marketCap',
-    orderDirection: 'desc',
-    limit: '50',
-    offset: '0'
-  },
-  headers: {
-    'X-RapidAPI-Key': '28bc40dbaemsheee61f352d0cb1cp1c568ejsndfbc4d6a0724',
-    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-  }
-};
+// const options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/coins',
+//   params: {
+//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//     timePeriod: '24h',
+//     'tiers[0]': '1',
+//     orderBy: 'marketCap',
+//     orderDirection: 'desc',
+//     limit: '50',
+//     offset: '0'
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': '28bc40dbaemsheee61f352d0cb1cp1c568ejsndfbc4d6a0724',
+//     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//   }
+// };
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
 
 function App() {
   return (
